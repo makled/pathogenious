198ba6e25436086044df04f0f379c3d69ce9f81f
lordvoldmort <newvoldmort@yahoo.com>
2016-11-13 15:44:14 +0000

    another trial 2016

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ router.get('/verify/:id', function(req, res, err) {
                 u.verified = true;
                 u.save(function(err) {
                     if (err) return console.log(err);
-                    res.redirect('http://pathogenious.me/reg_complete.html');
+                    res.redirect('http://pathogenious.kodr.in/reg_complete.html');
                 });
 
             });
@@ -412,7 +412,7 @@ io.on('connection', function(socket) {
                             from: 'pathogenious@gmail.com',
                             to: u.userName + "@guc.edu.eg",
                             subject: 'verify pathogenious',
-                            text: 'welcome ' + user.displayName + ' !!\n to verify your mail and unlock your account just click the following link : \n http://pathogenious.me/verify/' + t._id
+                            text: 'welcome ' + user.displayName + ' !!\n to verify your mail and unlock your account just click the following link : \n http://pathogenious.kodr.in/verify/' + t._id
                         }, function(err, info) {
                             if (err) {
                                 console.log("error sending verification mail with gmail ", err);
@@ -420,7 +420,7 @@ io.on('connection', function(socket) {
                                     from: 'pathogenious@pathogenious.me',
                                     to: u.userName + "@guc.edu.eg",
                                     subject: 'verify pathogenious',
-                                    text: 'welcome ' + user.displayName + ' !!\n to verify your mail and unlock your account just click the following link : \n http://pathogenious.me/verify/' + t._id
+                                    text: 'welcome ' + user.displayName + ' !!\n to verify your mail and unlock your account just click the following link : \n http://pathogenious.kodr.in/verify/' + t._id
                                 }, function(err, info) {
                                     if (err) return console.log("error sending mail with webfaction", err);
                                 });
@@ -837,7 +837,7 @@ io.on('connection', function(socket) {
             else {
                 transporterGmail.sendMail({
                     from: 'pathogenious@pathogenious.me',
-                    to: name + "@student.guc.edu.eg",
+                    to: name + "@guc.edu.eg",
                     subject: 'PathoGenius password request',
                     text: 'welcome ' + users[0].displayName + ' !!\n You requsted to get back your PathoGenius password . here it is !! : \n' + password
                 }, function(err, info) {
@@ -848,7 +848,7 @@ io.on('connection', function(socket) {
 
                         transporter.sendMail({
                             from: 'pathogenious@gmail.com',
-                            to: name + "@student.guc.edu.eg",
+                            to: name + "@guc.edu.eg",
                             subject: 'PathoGenius password request',
                             text: 'welcome ' + users[0].displayName + ' !!\n You requsted to get back your PathoGenius password . here it is !! : \n' + password
                         }, function(err, info) {
